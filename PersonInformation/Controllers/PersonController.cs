33f5aae4042b98e120c5d3b46f0986dbbfa340c8
Shourav Kumar <shourav45@gmail.com>
2017-09-16 18:06:22 +0600

    first commit

---
+++ PersonInformation/Controllers/PersonController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonInformation.Controllers
+{
+    using System.Security.Cryptography.X509Certificates;
+
+   
+
+    public class PersonController : Controller
+    {
+        public bool flag = false;
+        // GET: Person
+        public ActionResult Input()
+        {
+          
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Input( FormCollection frm)
+        {
+           
+            ViewBag.Name = frm["personname"].ToString();
+            ViewBag.Email = frm["personemail"].ToString();
+            ViewBag.Mobile = frm["personmobile"].ToString();
+            ViewBag.Address = frm["personaddress"].ToString();
+            
+            return View("Showinfo");
+        }
+
+        public ActionResult Showinfo(string name, string email, string mobile,string address)
+        {
+            ViewBag.Name = name;
+            ViewBag.Email = email;
+            ViewBag.Mobile = mobile;
+            ViewBag.Address = address;
+            return View(ViewBag);
+        }
+    }
+}
